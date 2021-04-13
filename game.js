48708f2429dbf4bcc26be4432ecb75c559675e42
PYJS123 <bibi.abrah@gmail.com>
2021-04-13 11:56:03 +0100

    Finishing off the drawing of the grid

--- game.js
+++ game.js
@@ -1,5 +1,34 @@
 // The actual game- the whole point
 
+var cnv, ctx;
+
+// Delayed selection of canvas- like async+await
+setTimeout(() => {
+  cnv = document.getElementById("JScanvas");
+  ctx = cnv.getContext("2d");
+}, 10);
+
 var gameLoop = function() {
-  // Game loop goes here
+  // This will be the default styling for line colour and width
+  ctx.strokeStyle = `rgb(${255-curr[0]}, ${255-curr[1]}, ${curr[2]}`;
+  ctx.lineWidth = 5;
+
+  // Drawing the lines
+  drawGrid();
+}
+
+// Code for drawing the grid
+var drawGrid = function() {
+  line(cnv.width/3*1, 0, cnv.width/3*1, cnv.height);
+  line(cnv.width/3*2, 0, cnv.width/3*2, cnv.height);
+  line(0, cnv.height/3*1, cnv.width, cnv.height/3*1);
+  line(0, cnv.height/3*2, cnv.width, cnv.height/3*2);
+}
+
+// Code for line- easier to read if it's a simplification
+var line = function(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
 }
