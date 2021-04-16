7abe5011ea45e0f1e13077d9c742b524bd047a9a
PYJS123 <bibi.abrah@gmail.com>
2021-04-16 12:42:05 +0100

    Writing some boilerplate code for the grid of cells

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 // The actual game- the whole point
 
+// Store the tic-tac-toe grid
+var grid = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+];
+
 
 var gameLoop = function() {
   // This will be the default styling for line colour and width
@@ -17,6 +24,21 @@ var mousePressed = function(event) {
 
 // Code for drawing the grid
 var drawGrid = function() {
+  drawLines();
+  drawTable(grid);
+}
+
+// Code to draw Xs, Os, and ""s
+var drawTable = function(table) {
+  for (let y = 0; y < table.length; y++) {
+    for (let x = 0; x < table[0].length; x++) {
+      // Draw the symbol here
+    }
+  }
+}
+
+// Code to draw lines
+var drawLines = function() {
   line(cnv.width/3*1, 0, cnv.width/3*1, cnv.height);
   line(cnv.width/3*2, 0, cnv.width/3*2, cnv.height);
   line(0, cnv.height/3*1, cnv.width, cnv.height/3*1);
